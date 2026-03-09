589710b0afc50aa13c79bfa3bfe8ea85ae640d88
Phạm Chí Trọng <122176490+TDev2910@users.noreply.github.com>
2026-03-09 23:06:46 +0700

    feat(ui): create top header (#9)
    Co-authored-by: peterho <hctd123@gmail.com>

--- resources/js/Pages/Frontend/Home/Index.vue
+++ resources/js/Pages/Frontend/Home/Index.vue
@@ -1,13 +1,8 @@
 <script setup>
-import FrontendLayout from '@/Layouts/FrontendLayout.vue';
-import Navbar from '@/Components/Frontend/Navbar.vue';
 </script>
 
 <template>
-    <FrontendLayout>
-        <Navbar />
-        <div>
-            Xin chào
-        </div>
-    </FrontendLayout>
+    <div>
+        Nội dung chính
+    </div>
 </template>
