55e8437aa00e4e090ba0b733f8147ba3c5291af8
DevPham <phamchitrong2910@gmail.com>
2026-03-23 02:41:53 +0700

    Enable PHP error display for debugging

--- api/index.php
+++ api/index.php
@@ -1,1 +1,6 @@
-﻿<?php require __DIR__ . "/../public/index.php";
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . "/../public/index.php";
